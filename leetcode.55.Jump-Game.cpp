6325f5b60ad808c1e83392618b80f5542ada8ffe
lambdacode.dev <lambdacode.dev@gmail.com>
2022-09-30 17:19:25 -0400

    leetcode.55.Jump-Game

---
+++ leetcode.55.Jump-Game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        //dp like: iterative from right end to left, and keep the least k for dp[k] = true
+        int least = nums.size()-1;
+        for(int j = nums.size() - 2; j >= 0; --j) {
+            if(j + nums[j] >= least)
+                least = j;
+        }
+        return least == 0;
+    }
+};
