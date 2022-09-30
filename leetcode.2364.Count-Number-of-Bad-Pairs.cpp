c0dba81246714463ff67e8d99a93bfbcd3e93b35
lambdacode.dev <lambdacode.dev@gmail.com>
2022-09-30 16:49:43 -0400

    leetcode.2364.Count-Number-of-Bad-Pairs

---
+++ leetcode.2364.Count-Number-of-Bad-Pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        //count good buddy groups: all have same i - nums[i] for any in the same good buddy group
+        for(int i = 0; i < nums.size(); ++i)
+            nums[i] -= i;
+        
+        unordered_map<int, int> groups;
+        for(int i = 0; i < nums.size(); ++i)
+            groups[nums[i]]++;
+        
+        long long goodpairs = 0;
+        for(auto const& [n, sz] : groups) {
+            goodpairs += (long long)(sz)*(sz-1)/2;
+        }
+        
+        return nums.size() * (nums.size()-1) / 2 - goodpairs;
+    }
+};
