9405a08e96b3712f8a2e3803f00f6811489605ab
lambdacode.dev <lambdacode.dev@gmail.com>
2022-09-30 17:22:10 -0400

    leetcode.17.Letter-Combinations-of-a-Phone-Number

---
+++ leetcode.17.Letter-Combinations-of-a-Phone-Number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    string letters[10] = {
+        "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz"
+    };
+    string digits;
+    vector<string> paths;
+    void dfs(int i, string& state) {
+        if(i == digits.size()) {
+            if(!state.empty())
+                paths.push_back(state);
+        }
+        else {
+            for(auto c : letters[digits[i]-'0']) {
+                state.push_back(c);
+                dfs(i+1, state);
+                state.pop_back();
+            }
+        }
+    }
+        
+public:
+    //recursive dfs with state variable (current partial string) update and back trace
+    vector<string> letterCombinations(string digits) {
+        this->digits = digits;
+        string state;
+        dfs(0, state); 
+        return paths;
+    }
+};
