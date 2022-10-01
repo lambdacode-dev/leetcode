9671db0a4c982030d7dc290e341d235ab7b00e2e
lambdacode.dev <lambdacode.dev@gmail.com>
2022-10-01 10:33:40 -0400

    added bash script to generate automatically readme

---
+++ generate_readme.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+stringZ=abcABC123ABCabc
+#       |------|
+#       12345678
+
+echo `expr "$stringZ" : 'abc[A-Z]*.2'`       # 8
+
+files=`ls -lcrt *cpp | sed 's/.* //'`
+echo "# leetcode" > README.md
+for f in $files; do
+    echo
+    echo $f
+    digits=`expr "$f" : '[0-9]*'`
+    problemid=${f:0:$digits}
+    let sz="${#f}-$digits-5" #without problem id and cpp ext
+    problem=${f:$digits+1:$sz}
+    url=`echo $problem | tr '[:upper:]' '[:lower:]'`
+    problem=`echo $problem | tr '-' ' '`
+    echo $problemid
+    echo $problem
+    echo "- [($problemid) $problem](https://leetcode.com/problems/$url/)" >> README.md
+done
+
