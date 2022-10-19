9efe28c91037e36a268c3c54e5eaf905640e2d31
lambdacode.dev <lambdacode.dev@gmail.com>
2022-10-19 16:45:03 -0400

    replace explicit type with auto

--- 328.Odd-Even-Linked-List.cpp
+++ 328.Odd-Even-Linked-List.cpp
@@ -4,8 +4,8 @@ public:
     //(time, space) = O(N) O(1)
     ListNode* oddEvenList(ListNode* head) {
         if(head) {
-            ListNode *odd = head, *evenhead = head->next;
-            for(ListNode *even = evenhead; even && even->next; ) {
+            auto odd = head, evenhead = head->next;
+            for(auto even = evenhead; even && even->next; ) {
                 odd = odd->next = even->next;
                 even = even->next = odd->next;
             }
