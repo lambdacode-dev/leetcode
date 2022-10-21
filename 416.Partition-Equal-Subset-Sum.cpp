3e99215dbc43d29bf7108c624981d6e0dfbb6cea
lambdacode.dev <lambdacode.dev@gmail.com>
2022-10-21 15:13:59 -0400

    416. Partition Equal Subset Sum

--- 416.Partition-Equal-Subset-Sum.cpp
+++ 416.Partition-Equal-Subset-Sum.cpp
@@ -3,7 +3,7 @@ public:
     //[iterative DP]: f_i(sum) -> bool (is there a subset of [0,i) with given sum?)
     //base: f_0(sum) = (sum == 0)
     //iteration f_i(sum) true => f_(i+1)(sum) true and f_(i+1)(sum + nums[i]) true
-    //(time, space) = O(N * S) O(S) where N is set size, and S is set total
+    //(time, space) = O(N*S) O(S) where N is set size, and S is set total
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if(sum & 0x01) return false;
