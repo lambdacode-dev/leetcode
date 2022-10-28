4addc97e1f536a379aecb4700826c473084ee973
lambdacode.dev <lambdacode.dev@gmail.com>
2022-10-28 15:59:46 -0400

    973.K-Closest-Points-to-Origin

---
+++ 973.K-Closest-Points-to-Origin.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    //[STL nth_element]
+    //(time,space) : O(N), O(1)
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        auto kth = points.begin() +k;
+        nth_element(points.begin(), kth, points.end(), [](auto const& p1, auto const& p2) {
+            return p1[0]*p1[0] + p1[1]*p1[1] < p2[0]*p2[0] + p2[1]*p2[1];
+        });
+        return {points.begin(), kth};
+    }
+};
