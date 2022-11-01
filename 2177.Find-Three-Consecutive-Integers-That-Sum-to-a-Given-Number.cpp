12f315aebe51d8639a40d5656d52a0a30bcf0d44
lambdacode.dev <lambdacode.dev@gmail.com>
2022-10-31 21:22:01 -0400

    more concise code

--- 2177.Find-Three-Consecutive-Integers-That-Sum-to-a-Given-Number.cpp
+++ 2177.Find-Three-Consecutive-Integers-That-Sum-to-a-Given-Number.cpp
@@ -3,8 +3,6 @@ public:
     //[math]: x = n-1 + n + n+1 = num
     //(time, space) = O(1), O(1)
     vector<long long> sumOfThree(long long num) {
-        if(num % 3)
-            return {};
-        return { num/3 - 1, num/3, num/3 + 1};
+        return num % 3 ? {} :{ num/3 - 1, num/3, num/3 + 1};
     }
 };
