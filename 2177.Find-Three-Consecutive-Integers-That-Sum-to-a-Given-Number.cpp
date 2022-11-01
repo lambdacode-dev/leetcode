58cb3d8368d0c9688f54d95b227a88e09438d74c
lambdacode.dev <lambdacode.dev@gmail.com>
2022-10-31 21:24:33 -0400

    Revert "more concise code"
    This reverts commit 12f315aebe51d8639a40d5656d52a0a30bcf0d44.

--- 2177.Find-Three-Consecutive-Integers-That-Sum-to-a-Given-Number.cpp
+++ 2177.Find-Three-Consecutive-Integers-That-Sum-to-a-Given-Number.cpp
@@ -3,6 +3,8 @@ public:
     //[math]: x = n-1 + n + n+1 = num
     //(time, space) = O(1), O(1)
     vector<long long> sumOfThree(long long num) {
-        return num % 3 ? {} :{ num/3 - 1, num/3, num/3 + 1};
+        if(num % 3)
+            return {};
+        return { num/3 - 1, num/3, num/3 + 1};
     }
 };
