971e2d448978ea45410348203d231ba9c1ef8683
lambdacode.dev <lambdacode.dev@gmail.com>
2022-11-03 16:21:52 -0400

    549. Binary Tree Longest Consecutive Sequence II: more concise code

--- 549.Binary-Tree-Longest-Consecutive-Sequence-II.cpp
+++ 549.Binary-Tree-Longest-Consecutive-Sequence-II.cpp
@@ -7,19 +7,14 @@ class Solution {
         function<array<int, 2>(TreeNode*)> dfs;//return length of  [0/1][dec/inc]-reasing path from root
         dfs = [&dfs, &longest] (TreeNode* node) {
             array<int,2> len = {1, 1};
-            if(node->right) {
-                array<int, 2> rlen = dfs(node->right);
-                if(node->right->val == node->val + 1)
-                    len[1] = 1 + rlen[1];
-                else if(node->right->val == node->val - 1)
-                    len[0] = 1 + rlen[0];
-            }
-            if(node->left) {
-                array<int, 2> llen = dfs(node->left);
-                if(node->left->val == node->val + 1)
-                    len[1] = max(len[1], 1 + llen[1]);
-                else if(node->left->val == node->val - 1)
-                    len[0] = max(len[0], 1 + llen[0]);
+            for(int i = 0; i < 2; ++i) {
+                if(auto child =  i ? node->right : node->left) {
+                    array<int, 2> childlen = dfs(child);
+                    if(child->val == node->val + 1)
+                        len[1] = max(len[1], 1 + childlen[1]);
+                    else if(child->val == node->val - 1)
+                        len[0] = max(len[0], 1 + childlen[0]);
+                }
             }
             longest = max(longest, len[0] + len[1] - 1);
             return len;
