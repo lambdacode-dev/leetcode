5dc11268540b2e4f861ab2971233c873f3468ade
lambdacode.dev <lambdacode.dev@gmail.com>
2022-11-05 22:35:23 -0400

    LFU finally O(1) algo

--- 460.LFU-Cache.cpp
+++ 460.LFU-Cache.cpp
@@ -1,43 +1,50 @@
 class LFUCache {
-    //[multiple frequency hashed doubly linked list]
-    //(time, space) = O(logN) O(N)
-    using FreqKeyVal = array<int, 3>;
-    map<int/*frequency*/, list<FreqKeyVal>> lists;
-    unordered_map<int/*key*/, list<FreqKeyVal>::iterator> dict;
+    //[list of list, hashing]: 1st level doubly linked list for same frequency, 2nd level single linked list in asending frequency order
+    //(time, space) = O(1) O(N)
+    struct KeyVal { int key; int val; };
+    using List = list<KeyVal>;// list of values with same frequency
+    struct ListAndFreq { list<KeyVal> list; int freq; };
+    using LofList = forward_list<ListAndFreq>;
+
+    LofList lofl;
+    unordered_map<int, pair<LofList::iterator, List::iterator>> dict;
     int capacity;
 
-    int& access(list<FreqKeyVal>::iterator itr) {
-        auto [freq, key, val] = *itr;
-        lists[freq++].erase(itr);
-        lists[freq].push_back({freq, key, val});
-        dict[key] = next(lists[freq].rbegin()).base();
-        return (*dict[key])[2];
+    int& access_and_update(int key, LofList::iterator i, List::iterator j) {
+        auto I = next(i);
+        if( I == lofl.end() || I->freq != i->freq + 1 )
+            I = lofl.insert_after( i, {List(), i->freq + 1} );
+
+        auto J = I->list.insert(I->list.end(), {key, j->val} );
+        dict[key] = {I, J};
+
+        i->list.erase(j);
+        return J->val;
     }
 public:
     LFUCache(int capacity) : capacity(capacity) {  }
 
     int get(int key) {
         auto itr = dict.find(key);
-        return itr != dict.end() ? access(itr->second) : -1;
+        return itr != dict.end() ? access_and_update(itr->first, itr->second.first, itr->second.second) : -1;
     }
 
     void put(int key, int value) {
         auto itr = dict.find(key);
-        if(itr != dict.end())
-            access(itr->second) = value;
+        if(itr != dict.end()) {
+            access_and_update(itr->first, itr->second.first, itr->second.second) = value;
+        }
         else if(capacity) {
             if(dict.size() == capacity) {
-                for(auto & [freq, lst] : lists) {
-                   if(!lst.empty()) {
-                       auto const& [freq, key, val] = lst.front();
-                       dict.erase(key);
-                       lst.pop_front();
-                       break;
-                   }
-                }
+                while(lofl.front().list.empty())
+                    lofl.pop_front();
+                dict.erase(lofl.front().list.front().key);
+                lofl.front().list.pop_front();
             }
-            lists[1].push_back({1, key, value});
-            dict[key] = next(lists[1].rbegin()).base();
+            if(lofl.empty() || lofl.front().freq != 1)
+                lofl.push_front( {List(), 1} );
+
+            dict[key] = { lofl.begin(), lofl.front().list.insert(lofl.front().list.end(), {key, value}) };
         }
     }
 };
