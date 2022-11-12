9c9620dddfa91f5d8cd46d59fc88d909a173d6f5
lambdacode.dev <lambdacode.dev@gmail.com>
2022-11-11 23:03:15 -0500

    make power3 one line

--- 326.Power-of-Three.cpp
+++ 326.Power-of-Three.cpp
@@ -6,28 +6,7 @@ class Solution {
     //(time, space) = O(1) O(1)
 public:
     bool isPowerOfThree(int n) {
-        constexpr int power3[20] = { //3^0 ... 3^19
-            Power3<0>::val,
-            Power3<1>::val,
-            Power3<2>::val,
-            Power3<3>::val,
-            Power3<4>::val,
-            Power3<5>::val,
-            Power3<6>::val,
-            Power3<7>::val,
-            Power3<8>::val,
-            Power3<9>::val,
-            Power3<10>::val,
-            Power3<11>::val,
-            Power3<12>::val,
-            Power3<13>::val,
-            Power3<14>::val,
-            Power3<15>::val,
-            Power3<16>::val,
-            Power3<17>::val,
-            Power3<18>::val,
-            Power3<19>::val
-            };
+        constexpr int power3[20] = { Power3<0>::val, Power3<1>::val, Power3<2>::val, Power3<3>::val, Power3<4>::val, Power3<5>::val, Power3<6>::val, Power3<7>::val, Power3<8>::val, Power3<9>::val, Power3<10>::val, Power3<11>::val, Power3<12>::val, Power3<13>::val, Power3<14>::val, Power3<15>::val, Power3<16>::val, Power3<17>::val, Power3<18>::val, Power3<19>::val };
 
         if(n < 0 || ! (n & 0b01) ) return false;
         auto i = lower_bound(power3, power3+20, n);
