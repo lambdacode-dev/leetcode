4c9a7e0dcccbf4c3a5adb591401bb495974bd342
lambdacode.dev <lambdacode.dev@gmail.com>
2022-12-12 23:44:16 -0500

    37. Sudoku Solver

--- 37.Sudoku-Solver.cpp
+++ 37.Sudoku-Solver.cpp
@@ -1,22 +1,17 @@
-//[backtrack on most restrictive cell]: at most 81^9 recursive calls.
-//(time, space) = O(1) O(1)
 class Solution {
+    //[backtrack on most restrictive cell]: at most 81^9 recursive calls.
+    //(time, space) = O(1) O(1)
 public:
     void solveSudoku(vector<vector<char>>& board) {
         //if [index] = true : the number 'index' is already used in the row/col/box
-        //[0] = count = how many trues
-        using Row = array<int,10>;
-        using Col = array<int,10>;
-        using Box = array<int,10>;
-        Row row[10] {};
-        Col col[10] {};
-        Box box[3][3] {};
+        using NumberUsed = array<bool,10>;
+        NumberUsed row[10] {}, col[10] {}, box[3][3] {};
         for(int r = 0; r < 9; ++r) {
             for(int c = 0; c < 9; ++c) {
                 if(board[r][c] != '.') {
-                    row[r][board[r][c] - '0'] = true; row[r][0]++;
-                    col[c][board[r][c] - '0'] = true; col[c][0]++;
-                    box[r/3][c/3][board[r][c] - '0'] = true; box[r/3][c/3][0]++;
+                    row[r][board[r][c] - '0'] = true;
+                    col[c][board[r][c] - '0'] = true;
+                    box[r/3][c/3][board[r][c] - '0'] = true;
                 }
             }
         }
@@ -40,22 +35,18 @@ public:
                     }
                 }
             }
-            if(R == -1 && C == -1) 
+            if(R == -1 && C == -1)
                 return true;
 
             for(int i = 1; i <= 9; ++i) {
                 if(!row[R][i] && !col[C][i] && !box[R/3][C/3][i]) {
                     board[R][C] = '0' + i;
-                    row[R][i] = true; row[R][0]++;
-                    col[C][i] = true; col[C][0]++;
-                    box[R/3][C/3][i] = true; box[R/3][C/3][0]++;
+                    row[R][i] = col[C][i] = box[R/3][C/3][i] = true;
 
                     if(backtrack()) return true;
 
                     board[R][C] = '.';
-                    row[R][i] = false; row[R][0]--;
-                    col[C][i] = false; col[C][0]--;
-                    box[R/3][C/3][i] = false; box[R/3][C/3][0]--;
+                    row[R][i] = col[C][i] = box[R/3][C/3][i] = false;
                 }
             }
             return false;
